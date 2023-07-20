6f76f0fa46c964676ee065a6f57f06c6609c40f2
cd C:\Users\nacho\OneDrive\Escritorio\ruby <nacho.nu02@gmail.com>
2023-07-19 21:06:58 -0300

    1.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from './game.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,11 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  title = 'final-app';
   result: string='';
   pointsUser = 0;
   pointsComp =  0;
 
+  reiniciarPagina(){
+    location.reload();
+  }
+
+
+
+  constructor(private playGame: GameService) {}
+
   ngOnInit(): void {
     this.result = 'Esperando jugada...';
+    console.log(this.pointsUser);
   }
+
+  play(choice: string): void {
+    const result = this.playGame.game(choice);
+    this.result = result.message;
+    this.pointsUser += result.userAdd;
+    this.pointsComp += result.compAdd;
+  }
+
+  
 }
